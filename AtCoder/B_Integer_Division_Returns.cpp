8171fc161f80f9be24af07191c3e4879a90eade8
Hasib-Nabid <nabid4424@gmail.com>
2024-03-16 19:57:49 +0600

    .

---
+++ AtCoder/B_Integer_Division_Returns.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define nabidtheboss() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define w(t) int t; cin >> t; cin.ignore(); while (t--) 
+
+int main()
+{
+    nabidtheboss();
+
+    ll k;
+long double n,c;
+cin>>n;
+	c = (n/10.00);
+	k = ceil(c);
+cout<<k<<endl;
+
+    return 0;
+}
