03364da84dcdbf28a5c80caf42c913d4f6455a4f
Hasib-Nabid <nabid4424@gmail.com>
2024-05-13 23:57:54 +0600

    .

---
+++ Codeforces/C_Clock_and_Strings.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define float double
+#define endl '\n'
+#define pb push_back
+#define po pop_back
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define shesh cout << endl
+#define loop(i,n) for (int i =0; i < (n); ++i)
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define gcd(a,b) __gcd(a,b)
+typedef vector<int> vi;
+int gcd ( int a, int b ) { return __gcd ( a, b ); }
+int lcm ( int a, int b ) { return a * ( b / gcd ( a, b ) ); }
+const double PI = acos(-1);
+#define nabidtheboss() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define w(t) int t; cin >> t; cin.ignore(); while (t--) 
+
+
+int32_t main () 
+{
+
+    nabidtheboss();
+    w(t){
+        int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    int c1=0,c2=0;
+    int s=max(a,b);
+    a=min(a,b);
+    b=s;
+    for(int i = a; i <= b; i++)
+    {
+        if(c==i) c1=1;
+        if(d==i) c2=1;
+    }
+    if((c1 and c2) or (!c1 and !c2)) no;
+    else yes;
+
+    }
+
+}
